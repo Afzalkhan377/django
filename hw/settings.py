a8656d69e75263c426de18971766b85b5d3bca79
Afzalkhan377 <97460290+Afzalkhan377@users.noreply.github.com>
2024-09-22 15:20:28 -0400

    updates

--- hw/settings.py
+++ hw/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o)@22%_*ga-s49zgi1+%=pu@-4hma42s!amngr__*kgx$wjuay
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['quoteapp.onrender.com', 'localhost']
+ALLOWED_HOSTS = ['quoteapp.onrender.com']
 
 
 # Application definition
